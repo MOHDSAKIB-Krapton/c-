78563c7246893e9d629762909af7feb926d97248
Mohd Sakib <mohdsakib@krapton.com>
2025-10-21 12:21:08 +0530

    Hello world

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Enter the value: ";
+    cin >> x;
+    cout << "Hello world!" << endl
+         << "The value of you provided is: " << x;
+    return 0;
+}
